d10a4e17d80d22d454d995dbec7b9815e59643a3
Renan <renanferraz.rw@gmail.com>
2025-09-09 13:49:40 -0300

    added selecao_model

--- main.py
+++ main.py
@@ -4,43 +4,19 @@ import os
 
 load_dotenv()
 cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-modelo = "gpt-4o"
-
-prompt_sistema = """
-        Você é um categorizador de produtos.
-        Você deve assumir as categorias presentes na lista abaixo.
-
-        # Lista de Categorias Válidas
-            - Moda Sustentável
-            - Produtos para o Lar
-            - Beleza Natural
-            - Eletrônicos Verdes
-
-        # Formato da Saída
-        Produto: Nome do Produto
-        Categoria: apresente a categoria do produto
-
-        # Exemplo de Saída
-        Produto: Escova elétrica com recarga solar
-        Categoria: Eletrônicos Verdes
-"""
-
-prompt_usuario = input("Apresente o nome de um produto: ")
 
 resposta = cliente.chat.completions.create(
     messages=[
         {
-            "role": "system",
-            "content": prompt_sistema
+            "role":"system",
+            "content" : "Listar apenas os nomes dos produtos, sem considerar descrição."
         },
         {
-            "role":"user",
-            "content": prompt_usuario
+            "role" : "user",
+            "content" : "Liste 3 produtos sustentáveis"
         }
     ],
-    model=modelo,
-    temperature = 0,
-    max_tokens = 200
+    model="gpt-4"
 )
 
 print(resposta.choices[0].message.content)
